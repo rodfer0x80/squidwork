e5aab3331b27d02dfc8655425d24d44b237aef65
rodfer0x80 <rodrigolf080@gmail.com>
2023-12-11 00:24:20 +0000

    delete pink square for linter

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 WORKDIR=~/.cache/$(basename $(pwd))
+PYTHON="$WORKDIR/.venv/bin/python" 
 test -e $WORKDIR/.venv ||\
     /usr/bin/python3 -m venv $WORKDIR/.venv &&\
-    $WORKDIR/.venv/bin/python -m pip install --upgrade pip &&\
-    $WORKDIR/.venv/bin/python -m pip install -r $PWD/requirements.txt
+    $PYTHON -m pip install --upgrade pip &&\
+    $PYTHON -m pip install -r $PWD/requirements.txt
