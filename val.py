a3158496ce365c835ca7993f4cdbf4cc31edb578
rodfer0x80 <rodrigolf080@gmail.com>
2023-12-11 19:29:33 +0000

    WIP[1]: separation of concerns

--- val.py
+++ val.py
@@ -31,9 +31,11 @@ class Listener:
     RATE = 16000
     CHUNK = 1600
 
-    def __init__(self):
+    def __init__(self, whisper_model_name="tiny.en"):
         nltk.download("punkt")
         Tensor.no_grad = True
+        self.model, self.enc = init_whisper(whisper_model_name)
+        self.is_listening_event = None
         pass
 
     @staticmethod
@@ -41,30 +43,49 @@ class Listener:
         for i in range(0, len(xs), n): yield xs[i:i + n]
 
     def listen(self):
-
-        pass
+        q = mp.Queue()
+        self.is_listening_event = mp.Event()
+        p = mp.Process(target=self.listener, args=(q, is_listening_event,))
+        p.daemon = True
+        p.start()
     
-    def listener(q: mp.Queue, event: mp.Event):
+    @staticmethod
+    def listener(self, q: mp.Queue, event: mp.Event):
         try:
             p = pyaudio.PyAudio()
-            stream = p.open(format=pyaudio.paInt16, channels=1, rate=RATE, input=True, frames_per_buffer=self.CHUNCK)
+            stream = p.open(format=pyaudio.paInt16, channels=1, rate=self.RATE, input=True, frames_per_buffer=self.CHUNCK)
             did_print = False
             while True:
-            data = stream.read(CHUNK) # read data to avoid overflow
-            if event.is_set():
-                if not did_print:
-                print("listening...")
-                did_print = True
-                q.put(((np.frombuffer(data, np.int16)/32768).astype(np.float32)*3))
-            else:
-                did_print = False
+                data = stream.read(self.CHUNK) # read data to avoid overflow
+                if event.is_set():
+                    if not did_print:
+                        print("listening...")
+                        did_print = True
+                    q.put(((np.frombuffer(data, np.int16)/32768).astype(np.float32)*3))
+                else:
+                    did_print = False
         finally:
             stream.stop_stream()
             stream.close()
             p.terminate()
     
+
+class Speaker:
+    # TODO: put this in a config file, only speak should be arg void
+    def __init__(self):
+        self.vits_model_to_use = "vctk"
+        self.vits_emotion_path = None
+        self.vits_speaker_id = 6
+        self.vits_seed = 1337
+        self.synth, self.emotion_embedding, self.text_mapper, self.hps, self.model_has_multiple_speakers = self.init_vits(self.vits_model_to_use, self.vits_emotion_path, self.vits_speaker_id, self.vits_seed)
+        self.vits_num_channels = 1
+        self.vits_noise_scale =0.8
+        self.vits_estimate_max_y_length = False
+        self.vits_noise_scale_w = None
+        self.out_counter = None
+
     @contextmanager
-    def output_stream(num_channels: int, sample_rate: int):
+    def output_stream(self, num_channels: int, sample_rate: int):
         try:
             p = pyaudio.PyAudio()
             stream = p.open(format=pyaudio.paInt16, channels=num_channels, rate=sample_rate, output=True)
@@ -76,21 +97,28 @@ class Listener:
             p.terminate()
 
     @staticmethod
-    def mp_output_stream(q: mp.Queue, counter: mp.Value, num_channels: int, sample_rate: int):
-        with output_stream(num_channels, sample_rate) as stream:
+    def mp_output_stream(self, q: mp.Queue, counter: mp.Value, num_channels: int, sample_rate: int):
+        with self.output_stream(num_channels, sample_rate) as stream:
             while True:
-            try:
-                stream.write(q.get())
-                counter.value += 1
-            except KeyboardInterrupt:
-                break
-
-class Speaker:
-    def __init__(self):
-        pass
+                try:
+                    stream.write(q.get())
+                    counter.value += 1
+                except KeyboardInterrupt:
+                    break
     
-    def speak():
-        pass
+    def speak(self):
+        out_q = mp.Queue()
+        self.out_counter = mp.Value("i", 0)
+        out_p = mp.Process(target=self.mp_output_stream, args=(out_q, self.out_counter, self.vits_num_channels,self. self.hps.data.sampling_rate,))
+        out_p.daemon = True
+        out_p.start()
+        for i in ["Hello, I'm a chat bot", "I am capable of doing a lot of things"]:
+            self.tts(
+                i, self.synth, self.hps, self.emotion_embedding,
+                self.vits_speaker_id, self.vits_model_to_use, self.vits_noise_scale,
+                self.vits_noise_scale_w, self.vits_length_scale,
+                self.vits_estimate_max_y_length, self.text_mapper, self.model_has_multiple_speakers
+            )
 
     def tts( 
         self,
@@ -166,12 +194,77 @@ class Speaker:
 
         return net_g, emotion_embedding, text_mapper, hps, model_has_multiple_speakers
 
-class Categoriser:
+
+class Agent:
     def __init__(self):
-        pass
+        self.speaker = Speaker()
+        self.listener = Listener()
+
+    # TODO: put this in logger class
+    @contextmanager
+    def log_writer(self):
+        try:
+            logs = []
+            yield logs
+        finally:
+            sep = "="*os.get_terminal_size()[1]
+            print(f"{sep[:-1]}\nCHAT LOG")
+            print(*logs, sep="\n")
+            print(sep)
+    
+    def chat(self):
+        with self.log_writer() as log:
+            while True:
+                tokens = [self.listener.enc._special_tokens["<|startoftranscript|>"], self.listener.enc._special_tokens["<|notimestamps|>"]]
+                total = np.array([])
+                self.speaker.out_counter.value = 0
+
+                s = time.perf_counter()
+                self.listener.is_listening_event.set()
+                prev_text = None
+        while True:
+            for _ in range(RATE // CHUNK): total = np.concatenate([total, q.get()])
+            txt = transcribe_waveform(model, enc, [total], truncate=True)
+            print(txt, end="\r")
+            if txt == "[BLANK_AUDIO]" or re.match(r"^\([\w+ ]+\)$", txt.strip()): continue
+            if prev_text is not None and prev_text == txt:
+            is_listening_event.clear()
+            break
+            prev_text = txtsxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+        print() # to avoid llama printing on the same line
+        log.append(f"{user_delim.capitalize()}: {txt}")
+
+        # Generate with llama
+        with Timing("llama generation: "):
+            outputted, start_pos, response = llama_generate(
+            llama, toks, outputted, txt, start_pos,
+            user_delim=user_delim, resp_delim=resp_delim, temperature=args.llama_temperature,
+            max_tokens=args.llama_count
+            )
+            log.append(f"{resp_delim.capitalize()}: {response}")
+
+        # Convert to voice
+        with Timing("tts: "):
+            sentences = nltk.sent_tokenize(response.replace('"', ""))
+            for i in sentences:
+            total = np.array([], dtype=np.int16)
+            for j in chunks(i.split(), args.max_sentence_length):
+                audio_data = tts(
+                " ".join(j), synth, hps, emotion_embedding,
+                args.vits_speaker_id, args.vits_model_to_use, args.vits_noise_scale,
+                args.vits_noise_scale_w, args.vits_length_scale,
+                args.vits_estimate_max_y_length, text_mapper, model_has_multiple_speakers
+                )
+                total = np.concatenate([total, audio_data])
+            out_q.put(total.tobytes())
+        while out_counter.value < len(sentences): continue
+        log.append(f"Total: {time.perf_counter() - s}")
 
     def parrot(self, data:str) -> str:
         return data
+
+    def predict(self, data:str) -> str:
+        return self.parrot(data)
 
 # def text_to_speech(text, output_path):
 #     command = f'tts --text "{text}" --out_path {output_path}'
